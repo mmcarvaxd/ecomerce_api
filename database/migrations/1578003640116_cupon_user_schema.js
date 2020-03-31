073550059260f9e3c8c5be475a17fdf5ccba2602
Matheus Carvalho <matheuscarva@hotmail.com>
2020-03-30 22:57:55 -0300

    Added Migration, CuponUser Schema

--- database/migrations/1578003640116_cupon_user_schema.js
+++ database/migrations/1578003640116_cupon_user_schema.js
@@ -4,16 +4,21 @@
 const Schema = use('Schema')
 
 class CuponUserSchema extends Schema {
-  up () {
-    this.create('cupon_users', (table) => {
-      table.increments()
-      table.timestamps()
-    })
-  }
+    up() {
+        this.create('cupon_users', (table) => {
+            table.increments()
+            table.integer('coupon_id').unsigned()
+            table.integer('user_id').unsigned()
+            table.timestamps()
 
-  down () {
-    this.drop('cupon_users')
-  }
+            table.foreign('order_id').references('id').inTable('order').onDelete('cascade')
+            table.foreign('user_id').references('id').inTable('users').onDelete('cascade')
+        })
+    }
+
+    down() {
+        this.drop('cupon_users')
+    }
 }
 
 module.exports = CuponUserSchema
