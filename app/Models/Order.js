3bc5587283bddd26d8681781003bf7732201e922
Matheus Carvalho <matheus.carvalho@mobilemed.com>
2020-04-10 11:40:36 -0300

    Impementado model de Order e OrderItem

---
+++ app/Models/Order.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Order extends Model {
+    itens() {
+        return this.hasMany('App/Models/OrderItem')
+    }
+    
+    cupons() {
+        return this.belongsToMany('App/Models/Cupon')
+    }
+
+    discounts() {
+        return this.hasMany('App/Models/Category')
+    }
+
+    user() {
+        return this.belongsTo('App/Models/User', 'user_id', 'id')
+    }
+}
+
+module.exports = Order
