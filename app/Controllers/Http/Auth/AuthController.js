64ab84b430748a2e427c75b1cfeacd74df404936
Matheus Carvalho <matheuscarva@hotmail.com>
2020-04-12 11:04:54 -0300

    implementado metodo de cadastrar usuario

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,9 +1,26 @@
 'use strict'
+const Database = use('Database')
+const User = use('App/models/user')
+const Role = use('Role')
 
 class AuthController {
 
     async register({ request, response }) {
-        //
+        const trx = await Database.beginTransaction()
+        
+        try {
+
+            const { name, surname, email, password } = request.call()
+            const user = await User.create({name, surname, email, password}, trx)
+            const userRole = await Role.findBy('slug', 'clent')
+            await user.roles().attach([userRole.id], null, trx)
+            await trx.commit()
+            return response.status(201).send({ data: user })
+        } catch (error) {
+
+            await trx.rollback()
+            return response.status(400).send({ message: 'Erro ao realizar cadastro!' })
+        }
     }
 
     async login({request, response, auth }) {
