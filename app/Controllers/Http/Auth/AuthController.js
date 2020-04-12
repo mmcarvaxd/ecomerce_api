4c105c893c1529798534cc0711c8f4a6959b3e22
Matheus Carvalho <matheuscarva@hotmail.com>
2020-04-12 11:14:53 -0300

    implementado metodo de login

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -7,11 +7,11 @@ class AuthController {
 
     async register({ request, response }) {
         const trx = await Database.beginTransaction()
-        
+
         try {
 
             const { name, surname, email, password } = request.call()
-            const user = await User.create({name, surname, email, password}, trx)
+            const user = await User.create({ name, surname, email, password }, trx)
             const userRole = await Role.findBy('slug', 'clent')
             await user.roles().attach([userRole.id], null, trx)
             await trx.commit()
@@ -23,8 +23,12 @@ class AuthController {
         }
     }
 
-    async login({request, response, auth }) {
-        //
+    async login({ request, response, auth }) {
+        const { email, password } = request.all()
+
+        let data = await auth.withRefreshToken().attempt(email, password)
+
+        return response.send({ data })
     }
 
     async refresh({ request, response, auth }) {
